5a5a309f167faaa4b9655f3160fb10495d8acd5c
phasna <phasnaaun69@gmail.com>
2025-04-04 16:11:23 +0200

    the best

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,15 @@ INSERT INTO city (name) VALUES ('Paris');
 INSERT INTO city (name) VALUES ('London');
 INSERT INTO city (name) VALUES ('New York');
 INSERT INTO city (name) VALUES ('Tokyo');
+INSERT INTO city (name) VALUES ('Cambodge');
+INSERT INTO city (name) VALUES ('Chine');
+INSERT INTO city (name) VALUES ('Japon');
+INSERT INTO city (name) VALUES ('Vietnam');
+INSERT INTO city (name) VALUES ('Thailand');
+INSERT INTO city (name) VALUES ('Inde');
+INSERT INTO city (name) VALUES ('Australie');
+INSERT INTO city (name) VALUES ('Nouvelle-Zélande');
+
 
 -- Insert airplanes
 INSERT INTO airplane (name) VALUES ('Boeing 747');
